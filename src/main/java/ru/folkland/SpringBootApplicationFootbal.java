e9332d4640016b9971f8da8979b8c5266eb49bee
folkland <medark92@gmail.com>
2019-09-19 08:19:05 +0300

    add spring boot

---
+++ src/main/java/ru/folkland/SpringBootApplicationFootbal.java
@@ -0,0 +1,22 @@
+package ru.folkland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @author folkland
+ */
+@SpringBootApplication
+public class SpringBootApplicationFootbal extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBootApplicationFootbal.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplicationFootbal.class, args);
+    }
+}
---
+++ src/main/java/ru/folkland/web/controllers/FootballControllers.java
@@ -0,0 +1,15 @@
+package ru.folkland.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @author folkland
+ */
+@EnableWebMvc
+@Controller
+@RequestMapping(path = "/football")
+public class FootballControllers {
+
+}
