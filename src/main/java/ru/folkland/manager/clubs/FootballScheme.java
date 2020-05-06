6e3d181c2e7e486355ac259fb5acf60ab4e740dc
U_M13DW <AHa58f3v>
2020-05-06 10:21:54 +0300

    refactoring FootballScheme enum class

--- src/main/java/ru/folkland/manager/clubs/FootballScheme.java
+++ src/main/java/ru/folkland/manager/clubs/FootballScheme.java
@@ -1,14 +1,37 @@
 package ru.folkland.manager.clubs;
 
+import lombok.Getter;
+import ru.folkland.manager.player.FootballPosition;
+
 /**
  * Схемы расстановки
  *
  * @author folkland
  */
+@Getter
 public enum FootballScheme {
-	s343,
-	s433,
-	s442,
-	s451,
-	s352
+	s343(3, 4, 3),
+	s433(4, 3, 3),
+	s442(4, 4, 2),
+	s451(4, 5, 1),
+	s352(3, 5, 2);
+
+	private final int defender;
+	private final int midfielder;
+	private final int forward;
+
+	FootballScheme(int defender, int midfielder, int forward) {
+		this.defender = defender;
+		this.midfielder = midfielder;
+		this.forward = forward;
+	}
+
+	int countOfPosition(FootballPosition position) {
+		switch (position) {
+			case defender: return defender;
+			case midfielder: return midfielder;
+			case forward: return forward;
+			default: return 1;
+		}
+	}
 }
